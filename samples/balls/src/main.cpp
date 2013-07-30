620cfe817b82197740b9f819ea91ae77d9336468
Alex <alex@acunliffe.com>
2013-07-30 01:12:09 +0100

    Encapsulating window, renderer and texture creation into functions

--- samples/balls/src/main.cpp
+++ samples/balls/src/main.cpp
@@ -29,6 +29,11 @@
 
 using namespace coment::samples::balls;
 
+// Forword declarations
+SDL_Window* createWindow(const char* title, int width, int height);
+SDL_Renderer* createRenderer(SDL_Window* window);
+SDL_Texture* createTexture(SDL_Renderer* renderer, int width, int height);
+
 // Window size
 int width = 800;
 int height = 600;
@@ -77,31 +82,13 @@ int main(int argc, char** argv)
 	lastFPSUpdate = lastUpdate;
 
 	// Create window
-	window = SDL_CreateWindow("Balls", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
-
-	if (window == NULL)
-	{
-		fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
-		return -1;
-	}
+	window = createWindow(TITLE_FORMAT, width, height);
 
 	// Create renderer
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	if (renderer == NULL)
-	{
-		fprintf(stderr, "Could not create renderer: %s\n", SDL_GetError());
-		return -1;
-	}
+	renderer = createRenderer(window);
 
 	// Create render texture
-	renderTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, width, height);
-
-	if (renderTexture == NULL)
-	{
-		fprintf(stderr, "Failed to create render texture: %s\n", SDL_GetError());
-		return -1;
-	}
+	renderTexture = createTexture(renderer, width, height);
 
 	// Create entity world
 	coment::World world;
@@ -260,4 +247,43 @@ int main(int argc, char** argv)
 	SDL_Quit();
 
 	return 0;
+}
+
+SDL_Window* createWindow(const char* title, int width, int height)
+{
+	SDL_Window* window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
+
+	if (window == NULL)
+	{
+		fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
+		exit(-1);
+	}
+
+	return window;
+}
+
+SDL_Renderer* createRenderer(SDL_Window* window)
+{
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	if (renderer == NULL)
+	{
+		fprintf(stderr, "Could not create renderer: %s\n", SDL_GetError());
+		exit(-1);
+	}
+
+	return renderer;
+}
+
+SDL_Texture* createTexture(SDL_Renderer* renderer, int width, int height)
+{
+	SDL_Texture* renderTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+
+	if (renderTexture == NULL)
+	{
+		fprintf(stderr, "Failed to create render texture: %s\n", SDL_GetError());
+		exit(-1);
+	}
+
+	return renderTexture;
 }
