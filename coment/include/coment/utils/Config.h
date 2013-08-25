b495863db071c6d293319552cd7fcf1c5abec167
Alex Cunliffe <alex@acunliffe.com>
2013-08-25 17:51:29 +0100

    Fixing g++ compatibility

--- coment/include/coment/utils/Config.h
+++ coment/include/coment/utils/Config.h
@@ -1,8 +1,10 @@
 #ifndef COMENT_CONFIG_H
 #define COMENT_CONFIG_H
 
-#if defined __clang__ && __has_include_next("unordered_map")
-#   define COMENT_CONFIG_HAS_CPP11
+#if defined __clang__
+#	if __has_include_next("unordered_map")
+#		define COMENT_CONFIG_HAS_CPP11
+#	endif
 #endif
 
 #endif /* COMENT_CONFIG_H */
