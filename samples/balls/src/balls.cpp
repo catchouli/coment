404599b2b0580c0e0203198d5f71e84ad898f594
Caitlin Wilks <cat@wilks.so>
2015-01-05 00:46:16 +0000

    Adding SDL2 and glew to balls project as well as code for initializing SDL2

--- samples/balls/src/balls.cpp
+++ samples/balls/src/balls.cpp
@@ -1,23 +1,80 @@
 #include <iostream>
-#include <memory>
-#include <cstdint>
+#include <SDL2/SDL.h>
 
-template <typename T1>
-class A
-{
-    template <typename T2>
-    void b(T2 t2);
-};
+#include <coment/World.h>
 
-template <typename T1>
-template <typename T2>
-void A<typename T1>::b(T2 t2)
-{
-
-}
+void initSDL(SDL_Window** window, SDL_Renderer** renderer);
+void deinitSDL(SDL_Window* window, SDL_Renderer* renderer);
 
 int main(int argc, char** argv)
 {
+    bool running = true;
 
-    system("pause");
+    // SDL stuff
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    // Initialise SDL
+    initSDL(&window, &renderer);
+
+    // Entity system
+    coment::World world;
+
+    // Main loop
+    while (running)
+    {
+        // Handle events
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            // Handle window closure
+            if ((event.type == SDL_QUIT) ||
+                (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE))
+            {
+                // Let the program exit after this iteration ends
+                running = false;
+            }
+        }
+
+        // Process entities
+
+    }
+
+    // Clean up
+    deinitSDL(window, renderer);
+
+    return 0;
+}
+
+void initSDL(SDL_Window** window, SDL_Renderer** renderer)
+{
+
+    // Initialise SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cerr << "Failed to initialise SDL: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+
+    // Create a window
+    *window = SDL_CreateWindow(
+        "Balls!",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        800, 600, SDL_WINDOW_SHOWN);
+
+    // Create a renderer
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+}
+
+void deinitSDL(SDL_Window* window, SDL_Renderer* renderer)
+{
+    // Clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 }
